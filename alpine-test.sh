92fecb1528dc089eea0e83131355e2c862261f54
uwegeercken <uwegeercken@web.de>
2020-04-09 10:07:25 +0200

    rework for more consistency

--- alpine-test.sh
+++ alpine-test.sh
@@ -9,20 +9,23 @@
 # image_registry_password >> the password to login to the private registry (for pushing images)
 
 image_base="alpine"
+image_base_version="latest"
+
 image_name="myalpine"
 image_version="1.0"
 image_format="docker"
 image_author="uwe.geercken@web.de"
+
+image_name_registry="${image_registry_docker_private}/${image_name}"
 image_tag="${image_registry_docker_private}/${image_name}:${image_version}"
 
 container_user="uwe"
 container_user_group="uwe"
 
-echo "[script] start of build: ${image_name}:${image_version}"
-container=$(buildah from ${image_registry_docker_group}/${image_base})
+echo "[script] start of build: ${image_name_registry}:${image_version}"
+container=$(buildah from ${image_registry_docker_group}/${image_base}:${image_base_version})
 
 echo "[script] working container: ${container}"
-
 buildah run $container addgroup -S "${container_user_group}"
 buildah run $container adduser -S "${container_user}" -G "${container_user_group}"
 buildah run $container chown root:${container_user} /opt
@@ -33,19 +36,19 @@ buildah config --author "${image_author}" $container
 buildah config --label name="${image_name}" $container
 buildah config --user "${container_user}" $container
 
-echo "[script] committing to image: ${image_registry_docker_private}/${image_name}"
-buildah commit --format "${image_format}" $container "${image_registry_docker_private}/${image_name}"
+echo "[script] committing to image: ${image_name_registry}"
+buildah commit --format "${image_format}" $container "${image_name_registry}"
 
 echo "[script] removing container: ${container}"
 buildah rm $container
 
-echo "[script] tagging ${image_name}: ${image_tag}"
-buildah tag  "${image_registry_docker_private}/${image_name}" "${image_tag}"
+echo "[script] tagging ${image_name_registry}: ${image_tag}"
+buildah tag  "${image_name_registry}" "${image_tag}"
 
 echo "[script] login to registry ${image_registry_docker_private}, using user: ${image_registry_user}"
 buildah login -u "${image_registry_user}" -p "${image_registry_password}" "${image_registry_docker_private}"
 
-echo "[script] pushing image ${image_name}:latest to: ${image_registry_docker_private}"
-buildah push --tls-verify=false "${image_registry_docker_private}/${image_name}" "docker://${image_registry_docker_private}/${image_name}"
-echo "[script] pushing image ${image_name}:${image_version} to: ${image_registry_docker_private}"
-buildah push --tls-verify=false "${image_registry_docker_private}/${image_name}:${image_version}" "docker://${image_tag}"
+echo "[script] pushing image ${image_name_registry}:latest to: ${image_registry_docker_private}"
+buildah push --tls-verify=false "${image_name_registry}" "docker://${image_name_registry}"
+echo "[script] pushing image ${image_tag} to: ${image_registry_docker_private}"
+buildah push --tls-verify=false "${image_tag}" "docker://${image_tag}"
